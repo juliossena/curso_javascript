cbc6d7e23c0559f5d4783c0aaab786a7e0b36a51
juliossena <juliosouzasena@gmail.com>
2023-04-22 00:01:19 -0300

    feat: aula-08

---
+++ aula-08.js
@@ -0,0 +1,6 @@
+const salario = '103';
+const salarioNumero = Number(salario);
+
+console.log('salario: ', salario);
+console.log('salario numero:', salarioNumero)
+console.log('salario numero:', salarioNumero.toFixed(2))
