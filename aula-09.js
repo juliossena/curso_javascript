7ccd1910759991bf0bc0eebf102b0ef84ebd660d
juliossena <juliosouzasena@gmail.com>
2023-04-22 00:07:58 -0300

    feat: aula 09

---
+++ aula-09.js
@@ -0,0 +1,12 @@
+
+function soma(item1, item2) {
+    return item1 + item2;
+}
+
+function diminuir(item1, item2) {
+    return item1 - item2;
+}
+
+console.log('SOMA: ', soma(5, 6));
+console.log('SOMA: ', soma(643, 64373));
+console.log('SOMA: ', diminuir(643, 64373));
