72fd2761dfc43a090f0379b0b8b03ae616ab4fe5
juliossena <juliosouzasena@gmail.com>
2023-04-21 23:30:01 -0300

    feat: aula-07

---
+++ aula-07.js
@@ -0,0 +1,12 @@
+const idadeJulio = 18;
+const idadeJoao = 14;
+const idadeJose = 53;
+
+const idades = [18, 'fdsaf', 53, 432, 654, 'fdgh', 234, 75, 765, 43, 543, 6754];
+
+for(let i = 0; i < idades.length; i++) {
+    console.log('resultado', idades[i]);
+}
+
+
+
