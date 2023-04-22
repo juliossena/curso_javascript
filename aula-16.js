42049ce7f798fe491be4038c9017f47d6e47e0e4
juliossena <juliosouzasena@gmail.com>
2023-04-22 12:02:21 -0300

    feat: aula-16

---
+++ aula-16.js
@@ -0,0 +1,14 @@
+const numero = 4324;
+const stringA = 'fdsafdsa';
+const verdadeOuFalso = true; // false
+
+const arrayTest = [432, 6436, 643];
+
+stringA[4] = 'JULIO';
+
+arrayTest.push(473985);
+
+console.log(arrayTest);
+console.log(stringA);
+
+
