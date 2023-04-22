69a4ed015cfdd5ee4d7b74a628dddb3f223c1685
juliossena <juliosouzasena@gmail.com>
2023-04-22 19:26:07 -0300

    feat: aula-20

---
+++ aula-20.js
@@ -0,0 +1,14 @@
+const idade = 654;
+
+const exibirIdade = () => {
+    const idade = 23;
+    console.log(idade);
+}
+
+const exibirExibirIDade = () => {
+    exibirIdade();
+    console.log(idade);
+}
+
+
+exibirExibirIDade();
