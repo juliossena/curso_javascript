80090a043a87203dbce6c7213f931208ffeed186
juliossena <juliosouzasena@gmail.com>
2023-04-23 14:18:08 -0300

    feat: aula-26

---
+++ aula-26.js
@@ -0,0 +1,48 @@
+
+const lista = [
+    {
+        nome: 'Jose',
+        idade: 13
+    },
+    {
+        nome: 'Joao',
+        idade: 42
+    },
+    {
+        nome: 'Maria',
+        idade: 23
+    },
+    {
+        nome: 'Gabriela',
+        idade: 25
+    },
+    {
+        nome: 'Fernanda',
+        idade: 65
+    },
+    {
+        nome: 'André',
+        idade: 69
+    },
+    {
+        nome: 'Heitor',
+        idade: 32
+    },
+    {
+        nome: 'Gustavo',
+        idade: 64
+    },
+    {
+        nome: 'Sandra',
+        idade: 33
+    },
+]
+
+const converterObjeto = (objeto) => {
+    return {
+        ...objeto,
+        idade: `${objeto.nome} + ${objeto.idade}`
+    }
+}
+
+console.log(lista.map(converterObjeto));
