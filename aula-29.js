29e257e29e294440d37fe59bd67958d326587540
juliossena <juliosouzasena@gmail.com>
2023-04-23 14:38:44 -0300

    feat: aula-29

---
+++ aula-29.js
@@ -0,0 +1,45 @@
+const lista = [
+    {
+        nome: 'Jose',
+        idade: 13,
+        exibir: true,
+    },
+    {
+        nome: 'Joao',
+        idade: 42
+    },
+    {
+        nome: 'Maria',
+        idade: 23
+    },
+    {
+        nome: 'Gabriela',
+        idade: 25
+    },
+    {
+        nome: 'Fernanda',
+        idade: 65
+    },
+    {
+        nome: 'André',
+        idade: 69
+    },
+    {
+        nome: 'Heitor',
+        idade: 32
+    },
+    {
+        nome: 'Gustavo',
+        idade: 64
+    },
+    {
+        nome: 'Sandra',
+        idade: 33
+    },
+]
+
+const buscarPessoa = (pessoa) => pessoa.nome === 'Fernanda';
+
+const novaPessoa = lista.find(buscarPessoa);
+
+console.log(novaPessoa);
