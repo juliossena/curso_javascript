ee03b36301317a562ed094418b72826767c737d4
juliossena <juliosouzasena@gmail.com>
2023-04-23 18:55:31 -0300

    feat: aula-31

---
+++ aula-31.js
@@ -0,0 +1,64 @@
+const lista = [
+    {
+        nome: 'Jose',
+        idade: 13,
+        exibir: true,
+    },
+    {
+        nome: 'Joao',
+        idade: 42
+    },
+    {
+        nome: 'Maria',
+        idade: 23
+    },
+    {
+        nome: 'Gabriela',
+        idade: 25
+    },
+    {
+        nome: 'Fernanda',
+        idade: 65
+    },
+    {
+        nome: 'André',
+        idade: 69
+    },
+    {
+        nome: 'Heitor',
+        idade: 32
+    },
+    {
+        nome: 'Gustavo',
+        idade: 64
+    },
+    {
+        nome: 'Sandra',
+        idade: 33
+    },
+]
+
+const funcaoReduce = (acc, objeto) => {
+    return {
+        ...acc,
+        [objeto.nome]: {
+            idade: objeto.idade
+        }
+    }
+}
+
+const pessoas = lista.reduce(funcaoReduce, {});
+
+const chaves = Object.keys(pessoas);
+
+const listaDeVolta = chaves.map((chave) => ({ 
+    nome: chave,
+    idade: pessoas[chave].idade
+}))
+
+// console.log(pessoas);
+
+// console.log(chaves);
+
+console.log(listaDeVolta);
+
